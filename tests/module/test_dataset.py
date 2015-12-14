022e7cfbd57ec6588a3da9429ea6c3651c0c1c3c
roll <roll@post.agency>
2015-12-14 18:12:40 +0300

    added dataset tests

--- tests/module/test_dataset.py
+++ tests/module/test_dataset.py
@@ -4,15 +4,34 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import pytest
 import unittest
+from mock import MagicMock, patch, ANY
 from importlib import import_module
 module = import_module('dpbq.dataset')
 
 
-# TODO: implement
-class DatasetTest(unittest.TestCase):
+class TestDataset(unittest.TestCase):
+
+    # Helpers
+
+    def setUp(self):
+
+        # Mocks
+        self.addCleanup(patch.stopall)
+        self.service = MagicMock()
+        self.project_id = 'project_id'
+        self.dataset_id = 'dataset_id'
+
+        # Create dataset
+        self.dataset = module.Dataset(
+                service=self.service,
+                project_id=self.project_id,
+                dataset_id=self.dataset_id)
 
     # Tests
 
-    def test(self):
-        self.assertTrue(module.Dataset)
+    def test___repr__(self):
+
+        # Assert values
+        assert repr(self.dataset)
